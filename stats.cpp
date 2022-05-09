aa731bd85244b349d076e1fa0fb51266e6d98d6c
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-05-09 17:14:16 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,5 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<float>& numb) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& VectorElements) {
     //Implement statistics here
 }
