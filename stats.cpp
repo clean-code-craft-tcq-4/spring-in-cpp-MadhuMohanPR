f45dfdec132fa8fcc75f834f935043849da2277d
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-05-14 22:40:42 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,33 +1,33 @@
 #include "stats.h"
-#include<cmath>
 #include<bits/stdc++.h>
 
-namespace Statistics {
-
+namespace Statistics
+{
 Stats::Stats()
 {
 }
 
-Stats::Stats(float avrg,float max,float min):Average(avrg),Maximum(max),Minimum(min)
+Stats::Stats(float avrg, float max, float min) : Average(avrg), Maximum(max), Minimum(min)
 {
 }
 
-Stats Stats::ComputeStatistics(const std::vector<float>& vectorElements)
+Stats Statistics::ComputeStatistics(const std::vector<float>& vectorElements)
 {
     //Implement statistics here
     Stats vElements;
     if(vectorElements.size()==0)
     {
-       vElements.Average=NAN;
-       vElements.Maximum=NAN;
-       vElements.Minimum=NAN;
+       vElements.Average = NAN;
+       vElements.Maximum = NAN;
+       vElements.Minimum = NAN;
     }
-
-    float Add         = accumulate(vectorElements.begin(), vectorElements.end() , 0);
-    vElements.Average = (Add/vectorElements.size());
-    vElements.Maximum = *max_element(vectorElements.begin(),vectorElements.end());
-    vElements.Minimum = *min_element(vectorElements.begin(),vectorElements.end());
-
+    else
+    {
+       vElements.Maximum = *max_element(vectorElements.begin(), vectorElements.end());
+       vElements.Minimum = *min_element(vectorElements.begin(), vectorElements.end());
+       float Add         = accumulate(vectorElements.begin(), vectorElements.end() , 0);
+       vElements.Average = (Add/vectorElements.size());
+    }
     return vElements;
 }
 
