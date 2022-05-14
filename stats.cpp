c8a3a65bacd3df71439c9a22780864f66b4f321f
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-05-14 12:59:21 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,29 @@
 #include "stats.h"
+#include<cmath>
+#include<bits/stdc++.h>
 
-Stats Statistics::ComputeStatistics(const std::vector<float>& VectorElements) {
+Statistics::Stats()
+{
+}
+Statistics::Stats(float avrg,float max,float min):Average(avrg),Maximum(max),Minimum(min)
+{
+}
+
+Stats Statistics::ComputeStatistics(const std::vector<float>& vectorElements)
+{
     //Implement statistics here
+    Stats vElements;
+    if(vectorElements.size()==0)
+    {
+       vElements.Average=NAN;
+       vElements.Maximum=NAN;
+       vElements.Minimum=NAN;
+    }
+
+    float Add         = accumulate(vectorElements.begin(), vectorElements.end() , 0);
+    vElements.Average = (Add/vectorElements.size());
+    vElements.Maximum = *max_element(vectorElements.begin(),vectorElements.end());
+    vElements.Minimum = *min_element(vectorElements.begin(),vectorElements.end());
+
+    return vElements;
 }
