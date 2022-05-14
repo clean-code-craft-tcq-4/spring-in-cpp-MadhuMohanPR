8b055ad74f89fb5cb882bbfa3cabc5b01a868133
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-05-15 01:19:07 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,26 +3,24 @@
 #include<bits/stdc++.h>
 using namespace Statistics;
 
-template <class T1>
-template <typename T=double>
-Statistics::Stats<T1>::Stats()
+template <class T1> Statistics::Stats<T1>::Stats()
 {
 }
 
-Statistics::Stats<T1>::Stats(const std::vector<T>& vectorElements) : vNumb({})
+template <typename T=double> template <class T1> Statistics::Stats<T1>::Stats(const std::vector<T>& vectorElements) : vNumb({})
 {
     vNumb.assign(vectorElements.begin(), vectorElements.end());
     Statistics::ComputeStatistics(vectorElements);
 }
 
-Statistics::Stats<T1>::Stats(T avrg, T max, T min) : Average(avrg), Maximum(max), Minimum(min)
+template <typename T=double> template <class T1> Statistics::Stats<T1>::Stats(T avrg, T max, T min) : Average(avrg), Maximum(max), Minimum(min)
 {
 }
 
-Statistics::Stats<T1> Statistics::ComputeStatistics(const std::vector<T>& vectorElements)
+template <typename T=double> template <class T1> Statistics::Stats<T1> Statistics::ComputeStatistics(const std::vector<T>& vectorElements)
 {
     //Implement statistics here
-    Statistics::Stats<T> vElements;
+    Statistics::Stats<T1> vElements;
     if(vectorElements.size()==0)
     {
        vElements.Average = NAN;
