46403aae95ae39a973ad04b6354f93e1dd2ed40e
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-05-15 13:05:32 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,7 +1,6 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
-#include "catch.hpp"
 #include "stats.h"
+#include "catch.hpp"
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #define SIZE 5
 
 TEST_CASE("reports average, minimum and maximum")
