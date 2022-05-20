a8d3936852cc301bc337c4aafc23b3067943d05b
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-05-20 18:58:43 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -4,7 +4,19 @@ namespace Statistics {
 Stats Statistics::ComputeStatistics(const std::vector<double>& vNumbers) {
     //Implement statistics here
     Stats vElements;
-    
+    if(vNumbers.size()==0)
+    {
+       vElements.average = NAN;
+       vElements.max     = NAN;
+       vElements.min     = NAN;
+    }
+    else
+    {
+       double Sum        = accumulate(vNumbers.begin(), vNumbers.end() , 0);
+       vElements.average = (Sum/vNumbers.size());
+       vElements.max     = *max_element(vNumbers.begin(), vNumbers.end());
+       vElements.min     = *min_element(vNumbers.begin(), vNumbers.end());
+    }
     return vElements;
 }
 }
