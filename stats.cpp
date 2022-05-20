14b159ad0966b26f54ce5d28fa22b219ef55e971
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-05-20 18:20:17 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,38 +1,5 @@
 #include "stats.h"
 
-template <class T1, class T2>
-namespace Statistics {
-   stats<T1>::stats()
-   {
-   }
-
-   stats<T1>::stats(const std::vector<T2>& vectorElements) : vNumb({})
-   {
-      vNumb.assign(vectorElements.begin(), vectorElements.end());
-      Statistics::ComputeStatistics(vectorElements);
-   }
-
-   stats<T1>::stats(T2 avrg, T2 max, T2 min) : Average(avrg), Maximum(max), Minimum(min)
-   {
-   }
-
-   stats<T1> Statistics::ComputeStatistics(const std::vector<T2>& vectorElements)
-   {
-       //Implement statistics here
-       Statistics::stats<T1> vElements;
-       if(vectorElements.size()==0)
-       {
-          vElements.Average = NAN;
-          vElements.Maximum = NAN;
-          vElements.Minimum = NAN;
-       }
-       else
-       {
-          vElements.Maximum = *max_element(vectorElements.begin(), vectorElements.end());
-          vElements.Minimum = *min_element(vectorElements.begin(), vectorElements.end());
-          T2 Add             = accumulate(vectorElements.begin(), vectorElements.end() , 0);
-          vElements.Average = (Add/vectorElements.size());
-       }
-       return vElements;
-   }
+Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+    //Implement statistics here
 }
