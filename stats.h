9d1c1d9ea76a2c876e5a44770d397c6d7c853c40
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-05-20 18:18:29 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,26 +1,5 @@
-#ifndef STATS_H
-#define STATS_H
-
-#include<iostream>
 #include <vector>
-#include<cmath>
-#include<bits/stdc++.h>
-#include<numeric>
-using namespace std;
 
-template <class T2>
 namespace Statistics {
-    class stats
-    {
-        public:
-           T2 Average;
-           T2 Maximum;
-           T2 Minimum;
-           const std::vector<T2>& vNumb;
-           stats();
-           stats(const std::vector<T2>& vectorElements);
-           stats(T2 avrg, T2 max, T2 min);
-    };
-
+    Stats ComputeStatistics(const std::vector<___>& );
 }
-#endif
