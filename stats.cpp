8f25aa66e496aa20cc570fbb9fb6f2fbaa7e052d
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-05-20 23:26:01 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,7 +1,7 @@
 #include "stats.h"
 
 namespace Statistics {
-Stats ComputeStatistics(const std::vector<double>& vNumbers) {
+Stats ComputeStatistics(const std::vector<float>& vNumbers) {
     //Implement statistics here
     Stats vElements;
     if(vNumbers.size()==0)
@@ -12,8 +12,7 @@ Stats ComputeStatistics(const std::vector<double>& vNumbers) {
     }
     else
     {
-       //double Sum        = accumulate(vNumbers.begin(), vNumbers.end() , 0.0);
-       vElements.average = (accumulate(vNumbers.begin(), vNumbers.end() , 0.0) / vNumbers.size());
+       vElements.average = (accumulate(vNumbers.begin(), vNumbers.end() , 0.000) / vNumbers.size());
        vElements.max     = *max_element(vNumbers.begin(), vNumbers.end());
        vElements.min     = *min_element(vNumbers.begin(), vNumbers.end());
     }
