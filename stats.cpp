7634080ad63b8d63343bb66dda37ffc1a3324223
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-05-20 23:19:35 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -12,8 +12,8 @@ Stats ComputeStatistics(const std::vector<double>& vNumbers) {
     }
     else
     {
-       double Sum        = accumulate(vNumbers.begin(), vNumbers.end() , 0);
-       vElements.average = (Sum/vNumbers.size());
+       //double Sum        = accumulate(vNumbers.begin(), vNumbers.end() , 0.0);
+       vElements.average = (accumulate(vNumbers.begin(), vNumbers.end() , 0.0) / vNumbers.size());
        vElements.max     = *max_element(vNumbers.begin(), vNumbers.end());
        vElements.min     = *min_element(vNumbers.begin(), vNumbers.end());
     }
