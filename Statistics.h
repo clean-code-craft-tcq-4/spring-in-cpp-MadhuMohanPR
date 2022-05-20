c70ceaca061c822fe54c0956b08067d576126cb9
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-05-20 17:34:59 +0530

    Create Statistics.h

---
+++ Statistics.h
@@ -0,0 +1,9 @@
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+#include <vector>
+namespace Statistics{
+template <class T1, class T2>
+  Stats<T1> ComputeStatistics(const std::vector<T2>& vectorElements);
+}
+#endif
