478fe5a4c8169e5ed93ce31b420441bfb596ce09
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-05-22 12:55:19 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,9 +2,10 @@
 #include <numeric>
 #include<algorithm>
 
-namespace Statistics {
-Stats ComputeStatistics(const std::vector<float>& vNumbers) {
-    //Implement statistics here
+namespace Statistics
+{
+Stats ComputeStatistics(const std::vector<float>& vNumbers)
+{
     Stats vElements;
     if(vNumbers.size()==0)
     {
@@ -23,38 +24,30 @@ Stats ComputeStatistics(const std::vector<float>& vNumbers) {
 } // namespace Statistics
 
 EmailAlert::EmailAlert():emailSent(false)
-{}
+{
+}
+
 LEDAlert::LEDAlert():ledGlows(false)
-{}
+{
+}
+
 IAlerter::IAlerter():emailAlert(NULL), ledAlert(NULL)
-{}
-StatsAlerter::StatsAlerter(float MaxThreshold, std::vector<IAlerter*> Alerters): IAlertPTR(NULL) //:emailAlertPTR(NULL), ledAlertPTR(NULL)
+{
+}
+
+StatsAlerter::StatsAlerter(float MaxThreshold, std::vector<IAlerter*> Alerters): IAlertPTR(NULL)
 {
     maxThreshold = MaxThreshold;
     IAlertPTR = Alerters.at(0);
-    //IAlertPTR->emailAlert = Alerters.at(0);
-    //IAlertPTR->ledAlert = Alerters.at(1);
-    //IAlertPTR->first = Alerters[0]->first;
-    //IAlertPTR->second = Alerters[0]->second;
-    //emailAlertPTR = Alerters.at(0);
-    //ledAlertPTR   = Alerters.at(1);
-    
-    //alerters.assign(Alerters.begin(), Alerters.end());
 }
+
 void StatsAlerter::checkAndAlert(const std::vector<float>& VctrNumbers)
 {
-    //float max = *VctrNumbers.rbegin();
     float max     = *max_element(VctrNumbers.begin(), VctrNumbers.end());
     if(max > maxThreshold)
     {
-        //IAlertPTR->emailAlert.emailSent = true;
-        //IAlertPTR->ledAlert.ledGlows = true;
         IAlertPTR->emailAlert->emailSent = true;
         IAlertPTR->ledAlert->ledGlows = true;
-        //IAlertPTR->first->emailSent = true;
-        //IAlertPTR->second->ledGlows = true;
-        //this->*emailAlertPTR.emailSent = true;
-        //(*ledAlertPTR).ledGlows    = true;
     }
     else
     {
